5d4e96d90f56ecdd2067034cc09e40c2e180af8a
Alvee3120 <fazlahkarim2002@gmail.com>
2025-07-23 02:36:06 +0600

    Initial commit

---
+++ task01/src/App.jsx
@@ -0,0 +1,11 @@
+
+
+function App() {
+  return (
+    <div><h1 class="text-3xl text-amber-200 font-bold underline">
+    Hello world!
+  </h1></div>
+  )
+}
+
+export default App
---
+++ task01/src/Text.jsx
@@ -0,0 +1,9 @@
+function Text(){
+    return(
+        <>
+        <h2 className="p-2 m-10 rounded-3xl text-xl text-white align-middle text-center bg-blue-800 ">We are here to Learn React from Tapascript</h2>
+        </>
+    )
+}
+
+export default Text 
